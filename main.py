d1b96efb0e5a5442d823ce5cf9cdfaa1f1917b0c
miketeee <tamichealwills@gmail.com>
2023-04-06 18:55:28 +0000

    Refactor transformation flow tasks.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     # Scrape S & P 500 data from wikipedia
     # add the data to a list
     # write the list to a text file
+
     extractionflows.get_s_and_p_500_companies()
     transformationflows.data_cleaning()
 
--- src/flows/transformationflows.py
+++ src/flows/transformationflows.py
@@ -1,10 +1,7 @@
 from prefect import flow
 from src.tasks.transformationtasks import *
-from datetime import datetime
 from dotenv import dotenv_values
-import requests
-import json
-import time
+
 
 api_key = dotenv_values()['API_KEY']
 
@@ -12,32 +9,8 @@ api_key = dotenv_values()['API_KEY']
 def data_cleaning():
     data_that_exists = verify_raw_data_file_exists()
     restructured_data = restructure_data(data_that_exists)
-
-    def get_ticker_balancesheet_json(ticker):
-        url = f"https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={ticker}&apikey={api_key}"
-        r = requests.request(method='GET', url=url)
-
-        try:
-            if r.json()['symbol']:
-                response_json = json.dumps(r.json(), indent=4)
-
-                with open(f"src/filestoprocess/balancesheets/{ticker}_balancesheet.json", mode='w') as output_file:
-                    output_file.write(response_json)
-                    print(r.json()['symbol'])
-
-        except KeyError:
-            print("You hit your per minute limit")
-            time.sleep(60)
-        finally:
-            pass
-
-    for company in restructured_data:
-        ticker = company[0]
-
-        get_ticker_balancesheet_json(ticker)
-
-
-
+    balance_sheet_retrieval(restructured_data, api_key)
+    write_to_csvs()
 
 
 
